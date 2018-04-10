4e7b5b039857e63a48db49a32c70668c1c4d623e
andrey-abramow <abramow.andrey@gmail.com>
2018-04-10 15:54:10 +0300

    fix config.js production settings

--- src/config.js
+++ src/config.js
@@ -54,7 +54,10 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/type-form-integration'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost/type-form-integration',
+      options: {
+        debug: false
+      }
     }
   }
 }
