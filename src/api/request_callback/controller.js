ef0ff404333deb3a0f51603c6a9dcab344076d06
andrey-abramow <abramow.andrey@gmail.com>
2018-04-13 16:56:16 +0300

    remove mocks from request controllers

--- src/api/request_callback/controller.js
+++ src/api/request_callback/controller.js
@@ -2,7 +2,7 @@ import callback_body              from './callback_body'
 import TypeFormRequestTransaction from '../../services/interactors/TypeFormRequestTransaction'
 
 export const create = ({ body }, res, next) => {
-  TypeFormRequestTransaction.call(callback_body,
+  TypeFormRequestTransaction.call(body,
     (err, result) => {
       console.log(err, result)
     }
