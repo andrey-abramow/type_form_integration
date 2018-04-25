b2022c54a0bf8a17764c66de46d9b6f5c60bd8d7
andrey-abramow <abramow.andrey@gmail.com>
2018-04-25 15:39:39 +0300

    change multy answers fields separator to new line

--- src/services/request_form/fields/RequestFormMultipleChoiceField.js
+++ src/services/request_form/fields/RequestFormMultipleChoiceField.js
@@ -6,14 +6,14 @@ class RequestFormMultipleChoiceField extends RequestFormNullField {
     if (this.answer.choice)
       return `${this.answer.choice.label}`
     else
-      return `${this.answer.choices.labels.join(', ')}`
+      return `${this.answer.choices.labels.join('\r\n')}`
   }
 
   to_s() {
     if (this.answer.choice)
       return `${this.field.title}: ${this.answer.choice.label}`
     else
-      return `${this.field.title}: ${this.answer.choices.labels.join(', ')}`
+      return `${this.field.title}: ${this.answer.choices.labels.join('\r\n')}`
 
   }
 }
